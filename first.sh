41e982eb5455b9a1a54897dd0818a19cff8d2a66
csuresh <sureshchilkuri@gmail.com>
2021-07-13 22:18:31 +0530

    Demo - echo & printf

---
+++ first.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+## Printing something on screen can be done by two commands
+# 1. echo
+# 2. printf
+
+# printf will have more syntaxes to deal with and however echo is more descriptive and yet does all the things that printf does.
+
+## Syntax: echo <MESSAGE TO PRINT >
+
+echo Hello World Suresh
+## Syntax for color or new lines : echo -e "MESSAGE\n\t\e"
+
+# echo -e option do need message to be quoted (Either single or double quotes) | (With out quotes also works but with more syntaxing)
+
+# Print Multiple Lines (\n), Some tab space (\t)
+
+echo -e Hello World,\n\tWelcome
